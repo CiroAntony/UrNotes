a6a4364978e8596d15e37e16bdeeccc41e02de85
CiroAntony <vasquezciro654@gmail.com>
2023-10-07 17:03:31 -0500

    responsive

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -64,44 +64,47 @@ const Notes = () => {
       >
         {isEditing ? "Cancelar" : "Nuevo"}
       </button>
-      <form
-        onSubmit={handleFormSubmit}
-        className={`bg-[#d19d10] pt-8 p-4 pb-8 flex flex-col gap-4 absolute left-[38.9%]
-        top-[20%] rounded-2xl text-white  ${isEditing ? "block" : "hidden"}`}
-      >
-        <div className="delete-container flex justify-end mt-[-20px]">
-          <TiDeleteOutline
-            onClick={handleCancelEdit}
-            className="text-[28px] text-white hover:text-green-900 cursor-pointer"
-          />
-        </div>
-        <h2 className="text-center text-lg font-semibold mt-[-20px]">
-          Ingresa o modifica tu nota
-        </h2>
-        <input
-          type="text"
-          value={editedNote.newNote}
-          onChange={(e) =>
-            setEditedNote({ ...editedNote, newNote: e.target.value })
-          }
-          placeholder="Ingrese el título"
-          className="p-2 w-[400px] outline-none rounded-2xl bg-[#2c2c2c]"
-        />
-        <textarea
-          value={editedNote.newDescript}
-          onChange={(e) =>
-            setEditedNote({ ...editedNote, newDescript: e.target.value })
-          }
-          placeholder="Ingrese la descripción"
-          className="w-[400px] h-[200px] p-2 rounded-xl outline-none bg-[#2c2c2c]"
-        />
-        <button
-          type="submit"
-          className="border-2 border-[#2c2c2c] rounded-2xl p-2 hover:bg-[#2c2c2c] duration-1000"
+      <div className="form-container flex justify-center">
+        <form
+          onSubmit={handleFormSubmit}
+          className={` form bg-[#d19d10] pt-8 p-4 pb-8 flex flex-col gap-4
+         rounded-2xl text-white  ${isEditing ? "block" : "hidden"}`}
         >
-          {isEditing ? "Guardar nota" : "Crear nota"}
-        </button>
-      </form>
+          <div className="delete-container flex justify-end mt-[-20px]">
+            <TiDeleteOutline
+              onClick={handleCancelEdit}
+              className="text-[28px] text-white hover:text-green-900 cursor-pointer"
+            />
+          </div>
+          <h2 className="text-center text-lg font-semibold mt-[-20px]">
+            Ingresa o modifica tu nota
+          </h2>
+          <input
+            type="text"
+            value={editedNote.newNote}
+            onChange={(e) =>
+              setEditedNote({ ...editedNote, newNote: e.target.value })
+            }
+            placeholder="Ingrese el título"
+            className="p-2 lg:w-[350px] sm:w-[350px] xl:w-[400px] outline-none rounded-2xl bg-[#2c2c2c]"
+          />
+          <textarea
+            type="text"
+            value={editedNote.newDescript}
+            onChange={(e) =>
+              setEditedNote({ ...editedNote, newDescript: e.target.value })
+            }
+            placeholder="Ingrese la descripción"
+            className=" textarea h-[200px] p-2 rounded-xl outline-none bg-[#2c2c2c]"
+          />
+          <button
+            type="submit"
+            className="border-2 border-[#2c2c2c] rounded-2xl p-2 hover:bg-[#2c2c2c] duration-1000"
+          >
+            {isEditing ? "Guardar nota" : "Crear nota"}
+          </button>
+        </form>
+      </div>
       <NotesList
         notes={notes}
         setNotes={setNotes}
